e8330e774783b23f21945596dabd28cacc1e2a98
aysucuk <aysusadixova@gmail.com>
2024-02-29 11:42:45 +0000

    98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - etry point
+ * @s: variable
+ * @b: variable
+ * @n: variable
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
