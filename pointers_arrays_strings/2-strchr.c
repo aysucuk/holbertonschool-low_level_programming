ae2b71d8a820ffa6e1a256551f6e1a0918336095
aysucuk <aysusadixova@gmail.com>
2024-03-04 12:21:32 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates
+ * @s: string
+ * @c: char
+ *
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 1;
+
+	while ((*(s + i) != c) && *(s + i))
+	{
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+
+	return (NULL);
+}
