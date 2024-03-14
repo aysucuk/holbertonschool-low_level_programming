06791db44e0205e1a6d793d4f6cd5b1eedfc46d5
aysucuk <aysusadixova@gmail.com>
2024-03-14 11:02:41 +0000

    Write a function that frees dogs.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
  * new_dog - creates new dog
  * @name: dog.name
@@ -13,26 +14,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
+        dog_t *dog = malloc(sizeof(dog_t));
 
-	if (dog == NULL)
-		return (NULL);
+        if (dog == NULL)
+                return (NULL);
 
-	(*dog).name = malloc(strlen(name) + 1);
-	if ((*dog).name == NULL)
-	{
-		free((*dog).name), free(dog);
-		return (NULL);
-	}
+        size_t name_len = 0;
+        size_t owner_len = 0;
 
-	(*dog).age = age;
+        char *temp_name = name;
+        char *temp_owner = owner;
 
-	(*dog).owner = malloc(strlen(owner) + 1);
-	if ((*dog).owner == NULL)
-	{
-		free((*dog).owner), free((*dog).name), free(dog);
-		return (NULL);
-	}
-	strcpy((*dog).name, name), strcpy((*dog).owner, owner);
-	return (dog);
+        while (*temp_name++ != '\0')
+                name_len++;
+        while (*temp_owner++ != '\0')
+                owner_len++;
+
+        (*dog).name = malloc(name_len + 1);
+        if ((*dog).name == NULL)
+        {
+                free(dog);
+                return (NULL);
+        }
+
+        (*dog).age = age;
+
+        (*dog).owner = malloc(owner_len + 1);
+        if ((*dog).owner == NULL)
+        {
+                free((*dog).name);
+                free(dog);
+                return (NULL);
+        }
+
+        strcpy((*dog).name, name);
+        strcpy((*dog).owner, owner);
+        return (dog);
 }
+
